1c10f06eb9ae8e4d665b8bd74fb561944744a168
LT <leetoplayaz@yandex.ru>
2023-08-17 15:09:54 +0300

    refactor: Many things
    Bad comment here

---
+++ Modal.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace MoonShine\UI;
+
+use Closure;
+use MoonShine\ActionButtons\ActionButtons;
+use MoonShine\Traits\Makeable;
+
+class Modal
+{
+    use Makeable;
+
+    protected array $buttons = [];
+
+    public function __construct(
+        protected ?Closure $title,
+        protected ?Closure $content
+    ) {
+    }
+
+    public function title(mixed $data = null): ?string
+    {
+        return call_user_func($this->title, $data);
+    }
+
+    public function content(mixed $data = null): ?string
+    {
+        return call_user_func($this->content, $data);
+    }
+
+    public function buttons(array $buttons): self
+    {
+        $this->buttons = $buttons;
+
+        return $this;
+    }
+
+    public function getButtons(): ActionButtons
+    {
+        return ActionButtons::make($this->buttons);
+    }
+}
