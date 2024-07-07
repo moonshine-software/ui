571d6eefa6408280ba4769a428ac015d36d806f1
LT <leetoplayaz@yandex.ru>
2024-07-07 13:44:45 +0300

    refactor: Flash to UI

---
+++ Components/Layout/Flash.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MoonShine\UI\Components\Layout;
+
+use MoonShine\UI\Components\MoonShineComponent;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+
+/**
+ * @method static static make(string $key = 'alert', string $type = 'info', bool $withToast = true, bool $removable = true)
+ */
+class Flash extends MoonShineComponent
+{
+    protected string $view = 'moonshine::components.layout.flash';
+
+    public function __construct(
+        protected string $key = 'alert',
+        protected string $type = 'info',
+        protected bool $withToast = true,
+        protected bool $removable = true,
+    ) {
+        parent::__construct();
+    }
+
+    /**
+     * @return array<string, mixed>
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    protected function viewData(): array
+    {
+        return [
+            'alert' => moonshine()->getRequest()->getSession($this->key),
+            'toast' => $this->withToast ? moonshine()->getRequest()->getSession('toast') : false,
+            'type' => $this->type,
+            'withToast' => $this->withToast,
+            'removable' => $this->removable,
+        ];
+    }
+}
